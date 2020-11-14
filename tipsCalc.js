b82ee861d18bf6567976a101d70a94c88f4d7bee
ftemplin <fritz.templin@gmail.com>
2020-11-14 12:13:26 -0800

    intitial commit

---
+++ tipsCalc.js
@@ -0,0 +1,10 @@
+function calculateTip (){
+	var	varBillAmount = document.querySelector(".billAmount");
+	var	varTip = document.querySelector(".tipPercent");
+	var	varSplitBy = document.querySelector(".splitBy");
+	console.log(varBillAmount);
+	console.log(varTip);
+	console.log(varSplitBy);
+
+}
+var test = "set this variable";
