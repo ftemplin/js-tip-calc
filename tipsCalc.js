396966104002980ac5700a8f40aedc01c44c1ede
ftemplin <fritz.templin@gmail.com>
2020-11-26 09:48:52 -0800

    Added external js Working results area needs CSS

--- tipsCalc.js
+++ tipsCalc.js
@@ -1,10 +1,13 @@
-function calculateTip (){
-	var	varBillAmount = document.querySelector(".billAmount");
-	var	varTip = document.querySelector(".tipPercent");
-	var	varSplitBy = document.querySelector(".splitBy");
-	console.log(varBillAmount);
-	console.log(varTip);
-	console.log(varSplitBy);
 
-}
-var test = "set this variable";
+ 	function calculateTip () {
+ 		let varBillAmount = Number(document.querySelector("#billAmount-input").value);
+ 		//console.log(varBillAmount)
+ 		let varTipPercent = Number(document.querySelector("#tipPercent-input").value);
+ 		//console.log(varTipPercent)
+ 		let varSplitBy = Number(document.querySelector("#splitBy-input").value);
+ 		//console.log(varSplitBy)
+ 		let varTipSplit = ((varBillAmount * (varTipPercent/100))/ varSplitBy);
+ 		//console.log(varTipSplit)
+ 		let USD = new Intl.NumberFormat("en-us",{style:"currency",currency:"USD"});
+ 		document.querySelector("#resultsOutput").innerHTML = "Each Person should pay " + USD.format(varTipSplit);
+ 		}
